64679c9e68b59f0627a12bee63a4c0c2a44f9980
makarai <17119942+ma-karai@users.noreply.github.com>
2020-04-16 15:07:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo "server.py start"
+
+git clone --depth 1 https://notabug.org/RemixDev/deemix.git $DEEMIX_HOME && \
+    rm -rfv $DEEMIX_HOME/.git
+    
+pip install -r $DEEMIX_HOME/requirements.txt
+
 python $DEEMIX_HOME/server.py
